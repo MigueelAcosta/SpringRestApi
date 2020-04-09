07bc3e4e3e6c9750bd2950e1efb20d43ad88d4ff
mangel1294 <noselaneta_12@hotmail.com>
2020-04-08 22:57:47 -0500

    first commit, project structure created

---
+++ lania-rest-api/src/main/java/com/miguel/apirest/LaniaRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaniaRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaniaRestApiApplication.class, args);
+	}
+
+}
