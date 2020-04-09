0da56dadf8b2026b564b4896162df5f0a18d0f8c
mangel1294 <noselaneta_12@hotmail.com>
2020-04-09 04:17:46 -0500

    old proyect deleted

--- MM/src/main/java/aplicacion/repositorios/CatEstatusRepository.java
+++
@@ -1,9 +0,0 @@
-package aplicacion.repositorios;
-
-import aplicacion.entidades.CatEstatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CatEstatusRepository extends JpaRepository<CatEstatus, Integer> {
-}
--- MM/src/main/java/aplicacion/servicios/CatalogoService.java
+++
@@ -1,8 +0,0 @@
-package aplicacion.servicios;
-
-import aplicacion.entidades.CatEstatus;
-import java.util.List;
-
-public interface CatalogoService {
-    public List<CatEstatus> getCatEstatus();
-}
